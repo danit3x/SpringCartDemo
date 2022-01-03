13b025208435232a2e60de3e576a6030910b9ced
danit3x <dvargas087@gmail.com>
2022-01-03 18:35:24 -0300

    inicial commit

---
+++ src/test/java/com/danito/CartDemo/CartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danito.CartDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
